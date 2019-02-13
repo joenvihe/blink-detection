82d4d4a4fd52ab3a4a8cefe3e3899a544c457691
Your Name <you@example.com>
2019-02-13 02:05:22 -0500

    x

--- detect_blinks_yawn.py
+++ detect_blinks_yawn.py
@@ -37,6 +37,7 @@ def eye_aspect_ratio(eye):
 # frames the eye must be below the threshold
 EYE_AR_THRESH = 0.24
 EYE_AR_CONSEC_FRAMES = 3
+YAWN_AR_THRESH = 32
 
 # initialize the frame counters and the total number of blinks
 COUNTER = 0
@@ -62,24 +63,83 @@ def imshow(img):
 	ax.set_xticks([]),ax.set_yticks([])
 	plt.show()
 
+def get_landmarks(im):
+	rects = detector(im, 1)
+
+	if len(rects) > 1:
+		return "error"
+	if len(rects) == 0:
+		return "error"
+	return np.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+
+def annotate_landmarks(im, landmarks):
+	im = im.copy()
+	for idx, point in enumerate(landmarks):
+		pos = (point[0, 0], point[0, 1])
+		#cv2.putText(im, str(idx), pos,
+		#			fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+		#			fontScale=0.25,
+		#			color=(0, 0, 255))
+		cv2.circle(im, pos, 1, color=(0, 255, 255))
+	return im
+
+
+def top_lip(landmarks):
+	top_lip_pts = []
+	for i in range(48, 54):
+		top_lip_pts.append(landmarks[i])
+	for i in range(60, 64):
+		top_lip_pts.append(landmarks[i])
+	top_lip_all_pts = np.squeeze(np.asarray(top_lip_pts))
+	top_lip_mean = np.mean(top_lip_pts, axis=0)
+	return int(top_lip_mean[:, 1])
+
+
+def bottom_lip(landmarks):
+	bottom_lip_pts = []
+	for i in range(65, 68):
+		bottom_lip_pts.append(landmarks[i])
+	for i in range(55, 59):
+		bottom_lip_pts.append(landmarks[i])
+	bottom_lip_all_pts = np.squeeze(np.asarray(bottom_lip_pts))
+	bottom_lip_mean = np.mean(bottom_lip_pts, axis=0)
+	return int(bottom_lip_mean[:, 1])
+
+
+def mouth_open(image):
+	landmarks = get_landmarks(image)
+
+	if landmarks == "error":
+		return image, 0
+
+	image_with_landmarks = annotate_landmarks(image, landmarks)
+	top_lip_center = top_lip(landmarks)
+	bottom_lip_center = bottom_lip(landmarks)
+	lip_distance = abs(top_lip_center - bottom_lip_center)
+	return image_with_landmarks, lip_distance
 
 fileStream = True
 time.sleep(1.0)
 
 cap = cv2.VideoCapture(0)
 # loop over frames from the video stream
+yawns = 0
+yawn_status = False
+
 while True:
 	# grab the frame from the threaded video file stream, resize
 	# it, and convert it to grayscale
 	# channels)
 	ret, frame = cap.read()
-	frame = imutils.resize(frame, width=1000)
+	frame = imutils.resize(frame, width=500)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	# add the equalize in image
 	gray = exposure.equalize_adapthist(gray, clip_limit=0.05)
 	data = 255 * gray
 	gray = data.astype(np.uint8)
+
 
 	# detect faces in the grayscale frame
 	rects = detector(gray, 0)
@@ -90,6 +150,27 @@ while True:
 		# convert the facial landmark (x, y)-coordinates to a NumPy
 		# array
 		shape = predictor(gray, rect)
+
+		#lips
+		landmarks = np.matrix([[p.x, p.y] for p in shape.parts()])
+		frame = annotate_landmarks(frame, landmarks)
+		top_lip_center = top_lip(landmarks)
+		bottom_lip_center = bottom_lip(landmarks)
+		lip_distance = abs(top_lip_center - bottom_lip_center)
+		print(lip_distance)
+
+		prev_yawn_status = yawn_status
+
+		if lip_distance > YAWN_AR_THRESH:
+			yawn_status = True
+		else:
+			if lip_distance > 10:
+				yawn_status = False
+
+		if prev_yawn_status == True and yawn_status == False:
+			yawns += 1
+			prev_yawn_status = False
+
 		shape = face_utils.shape_to_np(shape)
 
 		# extract the left and right eye coordinates, then use the
@@ -131,6 +212,8 @@ while True:
 		# the computed eye aspect ratio for the frame
 		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "Yawns: {}".format(str(yawns)), (150, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
